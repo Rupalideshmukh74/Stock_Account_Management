c213079c687b39e09a1b6d0e4552b64af91a7d47
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 19:26:40 +0530

    [Rupali]Feat:Added:welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stock_account_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Stock account management program!");
+        }
+    }
+}
