186bdfac43853b6a3a7e09115e2c0c0a8f4ee855
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-19 19:39:11 +0530

    [Rupali]Feat:Added:uploaded UC1

---
+++ Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_account_management
+{
+    class Stock
+    {
+        public string _stockName { get; set; }
+        public int _stockQuantity { get; set; }
+        public double _stockPrice { get; set; }
+        public double _stockTotalPrice { get; set; }
+    }
+}
